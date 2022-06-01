d122a6506c1a59a2a7fa1dd23dad306e7ef7b92c
starguys <kjj9500@naver.com>
2022-06-01 15:36:40 +0900

    2022-06-01 flutter study

--- studyapp/lib/main.dart
+++ studyapp/lib/main.dart
@@ -1,31 +1,105 @@
+import 'package:contacts_service/contacts_service.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(home: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   MyApp({Key? key}) : super(key: key);
 
-  var a = 1;
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  getPermission() async {
+    var status = await Permission.contacts.status;
+    if (status.isGranted) {
+      print('허락됨');
+      var contacts = await ContactsService.getContacts();
+      print(contacts[0].familyName);
+      name = contacts;
+    } else if (status.isDenied) {
+      print('거절됨');
+      Permission.contacts.request();
+      openAppSettings();
+    }
+  }
+
+  var name = [];
+
+  addName(a) {
+    setState(() {
+      name.add(a);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('연락처앱'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                getPermission();
+              },
+              icon: Icon(Icons.contacts))
+        ],
+      ),
+      body: ListView.builder(
+          itemCount: name.length,
+          itemBuilder: (c, i) {
+            return ListTile(title: Text(name[i].familyName));
+          }),
       floatingActionButton: FloatingActionButton(
-        child: Text('버튼!!!'),
+        child: Text('OK'),
         onPressed: () {
-          print(a);
-          a++;
+          showDialog(
+              context: context,
+              builder: (context) {
+                return DialogUI(addName: addName);
+              });
         },
       ),
-      appBar: AppBar(),
-      body: ListView.builder(
-          itemCount: 100,
-          itemBuilder: (c, i) {
-            return Text(i.toString());
-          }),
-    ));
+    );
+  }
+}
+
+class DialogUI extends StatelessWidget {
+  DialogUI({Key? key, this.addName}) : super(key: key);
+
+  final addName;
+
+  var inputData = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: SizedBox(
+        width: 300,
+        height: 300,
+        child: Column(
+          children: [
+            TextField(
+              controller: inputData,
+            ),
+            TextButton(
+                onPressed: () {
+                  addName(inputData.text);
+                  Navigator.pop(context);
+                },
+                child: Text('완료')),
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('취소'))
+          ],
+        ),
+      ),
+    );
   }
 }
