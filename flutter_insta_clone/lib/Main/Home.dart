a9587f2b02ba5eca03d9ffea6878241c501db076
starguys <kjj9500@naver.com>
2022-06-12 18:29:34 +0900

    insta clone study

---
+++ flutter_insta_clone/lib/Main/Home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 3,
+        itemBuilder: (c, i) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.network('https://codingapple1.github.io/app/car0.png'),
+              Text('like 100'),
+              Text('Editor'),
+              Text('Contents'),
+            ],
+          );
+        });
+  }
+}
---
+++ flutter_insta_clone/lib/style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+var theme = ThemeData(
+    iconTheme: IconThemeData(color: Colors.blue),
+    appBarTheme: AppBarTheme(
+        color: Colors.white,
+        elevation: 1,
+        titleTextStyle: TextStyle(color: Colors.black, fontSize: 25),
+        actionsIconTheme: IconThemeData(color: Colors.black)));
