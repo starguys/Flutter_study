2447b0be9cfdc88d08220d79823d3e9fb12b1642
Min <kjj9500@gmail.com>
2022-07-16 16:10:36 +0900

    hello_flutter add

---
+++ hello_flutter/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text(
+              'Hello Flutter',
+              style: TextStyle(fontSize: 28),
+            ),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: '이메일',
+                  ),
+                ),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: '비밀번호',
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.only(top: 24),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('로그인'),
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}
