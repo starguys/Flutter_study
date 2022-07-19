8e8e522e934f915782177ff773acc8e464c5d884
starguys <kjj9500@naver.com>
2022-07-19 13:04:16 +0900

    learn flutter

--- hello_flutter/lib/main.dart
+++ hello_flutter/lib/main.dart
@@ -19,30 +19,39 @@ class MyApp extends StatelessWidget {
               style: TextStyle(fontSize: 28),
             ),
           ),
-          body: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: '이메일',
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(32),
+                    child: Image.network(
+                      "https://i.ibb.co/CwzHq4z/trans-logo-512.png",
+                      width: 81,
+                    ),
                   ),
-                ),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: '비밀번호',
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: '이메일',
+                    ),
                   ),
-                ),
-                Container(
-                  width: double.infinity,
-                  margin: EdgeInsets.only(top: 24),
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text('로그인'),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: '비밀번호',
+                    ),
                   ),
-                )
-              ],
+                  Container(
+                    width: double.infinity,
+                    margin: EdgeInsets.only(top: 24),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('로그인'),
+                    ),
+                  )
+                ],
+              ),
             ),
           )),
     );
