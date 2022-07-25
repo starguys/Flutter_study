a49e7dcc6763e11ba94ef8372b8eea867a0752fd
아름드리 <arumdree@aleumdeuliui-MacBookPro.local>
2022-07-25 12:51:04 +0900

    learn flutter provider

--- bucket_list_with_provider/lib/bucket_servie.dart
+++ bucket_list_with_provider/lib/bucket_servie.dart
@@ -6,5 +6,23 @@ import 'main.dart';
 class BucketService extends ChangeNotifier {
   List<Bucket> bucketList = [
     Bucket('잠자기', false), // 더미(dummy) 데이터
+    Bucket('잠자기', false), // 더미(dummy) 데이터
+    Bucket('잠자기', false), // 더미(dummy) 데이터
   ];
+
+  /// bucket 추가
+  void createBucket(String job) {
+    bucketList.add(Bucket(job, false));
+    notifyListeners(); // 갱신 = Consumer<BucketService>의 builder 부분만 새로고침
+  }
+
+  void updateBucket(Bucket bucket, int index) {
+    bucketList[index] = bucket;
+    notifyListeners();
+  }
+
+  void deleteBucket(int index) {
+    bucketList.removeAt(index);
+    notifyListeners();
+  }
 }
